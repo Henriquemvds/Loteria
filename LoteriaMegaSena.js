7c2fc501ad5593b593d020575b3664c6dea95833
Henrique Mateus Vargas dos Santos <108635477+DevHenriquecido@users.noreply.github.com>
2022-07-17 21:49:34 -0300

    Update LoteriaMegaSena.js

--- LoteriaMegaSena.js
+++ LoteriaMegaSena.js
@@ -1,4 +1,5 @@
 
+
 function modoMegaSena(){
     document.getElementById('secJogos').style.display = 'none'
     document.getElementById('secLoteriaModoMegaSena').style.display = 'block'
@@ -40,21 +41,21 @@ function tenteLoteriaMegaSena(){
 
     let contador = 1
 for (let i = 0; sorteio.length >  i; i++){
-    document.getElementById("inpNumero1LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero2LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero3LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero4LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero5LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero6LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero7LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero8LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero9LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero10LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero11LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero12LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero13LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero14LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero15LoteriaMegaSena").style.backgroundColor = 'red'
+    numero1 = document.getElementById("inpNumero1LoteriaMegaSena").value
+    numero2 = document.getElementById("inpNumero2LoteriaMegaSena").value
+    numero3 = document.getElementById("inpNumero3LoteriaMegaSena").value
+    numero4= document.getElementById("inpNumero4LoteriaMegaSena").value
+    numero5 = document.getElementById("inpNumero5LoteriaMegaSena").value
+    numero6 = document.getElementById("inpNumero6LoteriaMegaSena").value
+    numero7 = document.getElementById("inpNumero7LoteriaMegaSena").value
+    numero8 = document.getElementById("inpNumero8LoteriaMegaSena").value
+    numero9 = document.getElementById("inpNumero9LoteriaMegaSena").value
+    numero10 = document.getElementById("inpNumero10LoteriaMegaSena").value
+    numero11 = document.getElementById("inpNumero11LoteriaMegaSena").value
+    numero12 = document.getElementById("inpNumero12LoteriaMegaSena").value
+    numero13 = document.getElementById("inpNumero13LoteriaMegaSena").value
+    numero14 = document.getElementById("inpNumero14LoteriaMegaSena").value
+    numero15 = document.getElementById("inpNumero15LoteriaMegaSena").value
 
 
     if (numero1 == sorteio[i]){
@@ -117,6 +118,7 @@ for (let i = 0; sorteio.length >  i; i++){
         acertos++
         document.getElementById("inpNumero15LoteriaMegaSena").style.backgroundColor = 'green'
     }
+}
     if(acertos < 5){
         document.getElementById('divResultadosMegaSena').innerHTML = 'Resultados: ' + acertos + ' acertos!'
     }
@@ -132,7 +134,7 @@ for (let i = 0; sorteio.length >  i; i++){
     if (contador >= 1){
         document.getElementById("botaoLoteriaMegaSena").disabled = true;
     }
-}
+
 
 }
 
@@ -157,37 +159,38 @@ function tenteDeNovoLoteriaMegaSena(){
     i = i + 1
     }
 
-    document.getElementById("inpNumero1LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero2LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero3LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero4LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero5LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero6LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero7LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero8LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero9LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero10LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero11LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero12LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero13LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero14LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero15LoteriaMegaSena").style.backgroundColor = 'red'
+    document.getElementById("inpNumero1LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero2LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero3LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero4LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero5LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero6LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero7LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero8LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero9LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero10LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero11LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero12LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero13LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero14LoteriaMegaSena").value = ''
+    document.getElementById("inpNumero15LoteriaMegaSena").value = ''
+    
+    document.getElementById("inpNumero1LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero2LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero3LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero4LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero5LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero6LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero7LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero8LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero9LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero10LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero11LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero12LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero13LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero14LoteriaMegaSena").style.backgroundColor = 'white'
+    document.getElementById("inpNumero15LoteriaMegaSena").style.backgroundColor = 'white'
 
-    document.getElementById("inpNumero1LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero2LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero3LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero4LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero5LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero6LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero7LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero8LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero9LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero10LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero11LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero12LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero13LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero14LoteriaMegaSena").style.backgroundColor = 'red'
-    document.getElementById("inpNumero15LoteriaMegaSena").style.backgroundColor = 'red'
 
 
 }
